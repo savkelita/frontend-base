6277502bd5494ce1e4b4c6609d8a3b84188e7c16
Marko Savic <marko.savic@yottasolution.rs>
2026-08-28 16:15:56 +0200

    feat: 404, 401 UI

--- src/router/components/not-found-view.tsx
+++ src/router/components/not-found-view.tsx
@@ -1,8 +1,53 @@
-import { Card, CardHeader, Title1, Text } from '@fluentui/react-components'
+import { Body1, Button, Caption1, Card, Title2, makeStyles, tokens } from '@fluentui/react-components'
+import { DocumentQuestionMarkRegular, HomeRegular } from '@fluentui/react-icons'
 
-export const NotFoundView = ({ path }: { path: string }) => (
-  <Card>
-    <CardHeader header={<Title1>404</Title1>} />
-    <Text>Page &quot;{path}&quot; not found</Text>
-  </Card>
-)
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    padding: tokens.spacingVerticalXXL,
+  },
+  card: {
+    width: '440px',
+    maxWidth: '100%',
+    alignItems: 'center',
+    textAlign: 'center',
+    rowGap: tokens.spacingVerticalS,
+    padding: tokens.spacingVerticalXXL,
+  },
+  ikona: {
+    fontSize: '48px',
+    color: tokens.colorNeutralForeground4,
+  },
+  kod: {
+    color: tokens.colorNeutralForeground3,
+  },
+  adresa: {
+    color: tokens.colorNeutralForeground3,
+    overflowWrap: 'anywhere',
+  },
+  dugme: {
+    marginTop: tokens.spacingVerticalM,
+  },
+})
+
+export const NotFoundView = ({ path }: { path: string }) => {
+  const styles = useStyles()
+
+  return (
+    <div className={styles.root}>
+      <Card className={styles.card}>
+        <DocumentQuestionMarkRegular className={styles.ikona} />
+        <Caption1 className={styles.kod}>404</Caption1>
+        <Title2>Strana ne postoji</Title2>
+        <Body1>Adresa koju ste otvorili ne postoji ili je u medjuvremenu promenjena.</Body1>
+        <Body1 className={styles.adresa}>{path}</Body1>
+        <Button className={styles.dugme} appearance="primary" icon={<HomeRegular />} as="a" href="/">
+          Idi na pocetnu
+        </Button>
+      </Card>
+    </div>
+  )
+}
--- src/router/components/unauthorized-view.tsx
+++ src/router/components/unauthorized-view.tsx
@@ -1,8 +1,53 @@
-import { Card, CardHeader, Title1, Text } from '@fluentui/react-components'
+import { Body1, Button, Caption1, Card, Title2, makeStyles, tokens } from '@fluentui/react-components'
+import { HomeRegular, ShieldProhibitedRegular } from '@fluentui/react-icons'
 
-export const UnauthorizedView = ({ path }: { path: string }) => (
-  <Card>
-    <CardHeader header={<Title1>401</Title1>} />
-    <Text>You do not have permission to access &quot;{path}&quot;</Text>
-  </Card>
-)
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    padding: tokens.spacingVerticalXXL,
+  },
+  card: {
+    width: '440px',
+    maxWidth: '100%',
+    alignItems: 'center',
+    textAlign: 'center',
+    rowGap: tokens.spacingVerticalS,
+    padding: tokens.spacingVerticalXXL,
+  },
+  ikona: {
+    fontSize: '48px',
+    color: tokens.colorNeutralForeground4,
+  },
+  kod: {
+    color: tokens.colorNeutralForeground3,
+  },
+  adresa: {
+    color: tokens.colorNeutralForeground3,
+    overflowWrap: 'anywhere',
+  },
+  dugme: {
+    marginTop: tokens.spacingVerticalM,
+  },
+})
+
+export const UnauthorizedView = ({ path }: { path: string }) => {
+  const styles = useStyles()
+
+  return (
+    <div className={styles.root}>
+      <Card className={styles.card}>
+        <ShieldProhibitedRegular className={styles.ikona} />
+        <Caption1 className={styles.kod}>401</Caption1>
+        <Title2>Nemate pravo pristupa</Title2>
+        <Body1>Ova strana trazi ovlascenje koje vas nalog nema. Obratite se administratoru.</Body1>
+        <Body1 className={styles.adresa}>{path}</Body1>
+        <Button className={styles.dugme} appearance="primary" icon={<HomeRegular />} as="a" href="/">
+          Idi na pocetnu
+        </Button>
+      </Card>
+    </div>
+  )
+}
